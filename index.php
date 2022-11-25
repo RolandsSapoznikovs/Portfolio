6fda1fc8d0e325a1308caa390161abf2c04ea222
Rolands <rolands_sapoznikovs@inbox.lv>
2022-11-25 15:06:40 +0200

    More php testing

--- index.php
+++ index.php
@@ -10,15 +10,10 @@
     <h1> Hello World </h1>
     
     <?php
-    function myTest() {
-        static $x = 0;
-        echo $x;
-        $x++;
-      }
-      
-      myTest();
-      myTest();
-      myTest();
+    echo "<h2>PHP is Fun!</h2>";
+    echo "Hello world!<br>";
+    echo "I'm about to learn PHP!<br>";
+    echo "This ", "string ", "was ", "made ", "with multiple parameters.";
     ?>
 </body>
 </html>
