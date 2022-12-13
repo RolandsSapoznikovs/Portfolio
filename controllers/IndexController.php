c1b9c6182b3dc0c7934c87cdbeaa401eeecb80d7
Rolands <rolands_sapoznikovs@inbox.lv>
2022-12-13 17:52:03 +0200

    Restart

--- controllers/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class IndexController
-    {
-        public function indexAction()
-        {
-            require 'C:/laragon/www/Portfolio/views/index.phtml';
-        }
-    }
-?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <div class="header">My Portfolio</div>
+</body>
+</html>
