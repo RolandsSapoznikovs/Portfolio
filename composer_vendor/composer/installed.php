b5cc94c74b59b0e14afe71ea6cdd9196eb4c7314
Rolands <rolands_sapoznikovs@inbox.lv>
2022-12-13 13:35:38 +0200

    Adding composer_vendor

---
+++ composer_vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rolands/portfolio',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '936dedaeca556ee8dfdbc53bb42c725fb812b254',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'erusev/parsedown' => array(
+            'pretty_version' => '1.7.4',
+            'version' => '1.7.4.0',
+            'reference' => 'cb17b6477dfff935958ba01325f2e8a2bfa6dab3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../erusev/parsedown',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rolands/portfolio' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '936dedaeca556ee8dfdbc53bb42c725fb812b254',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
