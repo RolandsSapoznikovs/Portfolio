4ab60afcd4cd24daac0d0559d65a13a52b0c79b5
Rolands <rolands_sapoznikovs@inbox.lv>
2022-12-13 17:40:26 +0200

    Fixing paths

--- public/index.php
+++ public/index.php
@@ -4,12 +4,12 @@ require 'C:/laragon/www/Portfolio/controllers/IndexController.php';
 require 'C:/laragon/www/Portfolio/controllers/RegisterController.php';
 if (!isset($_GET['controller']))
 {
-    $controllerName = 'C:/laragon/www/Portfolio/controllers\\IndexController';
+    $controllerName = 'C:\laragon\www\Portfolio\controllers\\IndexController';
     $action         = 'indexAction';
 }
 else
 {
-    $controllerName     = 'C:/laragon/www/Portfolio/controllers\\' . ucfirst($_GET['controller']) . 'Controller';
+    $controllerName     = 'C:\laragon\www\Portfolio\controllers\\' . ucfirst($_GET['controller']) . 'Controller';
     $action             = $_GET['action'] . 'Action';
 }
 $controller = new $controllerName();
